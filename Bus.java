9ac7af00cec4dcb24fc1fbf93a8f203cc7b8d428
iyumili0631 <118509722+iyumili0631@users.noreply.github.com>
2022-12-24 15:14:29 +0800

    update

--- Bus.java
+++ Bus.java
@@ -1,3 +1,30 @@
-public class Bus {
+import java.util.ArrayList;
+
+public class Transportation extends Bus {
+    String name = " ";
+    Route route = new Route (ArrayList <Station> stop, Time startOperation, Time finishOperation, Time gap);
+    
+    public Bus (String name, int speed, ArrayList <Station> stop, Time startOperation, Time finishOperation, Time gap){
+        this.name = name;
+        speed = 5;
+    }
+    
+    public String getName (){
+        return name;
+    }
+    
+    public boolean operate (Time time){
+        
+    }
+    
+    public boolean stop (Station station){
+        
+    }
+    
+    public Time waitNextBus (Time now){
+        
+    }
+    
+    
     
 }
