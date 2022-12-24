ef82ccbdad8195cdd21d1ce0969af81f3c35d5e8
iyumili0631 <118509722+iyumili0631@users.noreply.github.com>
2022-12-24 22:27:29 +0800

    update

--- Route.java
+++ Route.java
@@ -10,18 +10,18 @@ public class Route {
   }
   
   public Time getStationATimeTable (){
-    Time A ;
+    Time A;
     int timeA = 480;
-    if (bus.getName() == "first"){
+    if (bus.getName().equals("first")){
       while (timeA <= 1080){
         timeA += 20;
         A = A.howManyMinute(timeA);
       }
     }
     
-    if (bus.getName() == "second"){
-      while (time <= 1080){
-        time += 30;
+    if (bus.getName().equals("second")){
+      while (timeA <= 1080){
+        timeA += 30;
         A = A.howManyMinute(timeA);
       }
     }
@@ -30,19 +30,56 @@ public class Route {
   }
   
   public Time getStationBTimeTable (){
+    Time B;
+    int timeB = 485;
+    while (timeB <= 1085){
+      timeB += 20;
+      B = B.howManyMinute(timeB);
+    }
     
+    return B;
   }
   
   public Time getStationCTimeTable (){
+    Time C;
+    int timeC = 490;
+    if (bus.getName().equals("first")){
+      while (timeC <= 1090){
+        timeC +=20;
+        C = C.howManyMinute(timeC);
+      }
+    }
     
+    if (bus.getName().equals("second")){
+      while (timeC <= 1090){
+        timeC += 30;
+        C = C.howManyMinute(timeC);
+      }
+    }
+    
+    return C;
   }
   
   public Time getStationDTimeTable (){
+    Time D;
+    int timeD = 495;
+    while (timeD <= 1095){
+      timeD += 20;
+      D = D.hoeManyMinute(timeD);
+    }
     
+    return D;
   }
   
   public Time getStationETimeTable (){
+    Time E;
+    int timeE = 500;
+    while (timeE <= 1100){
+      timeE += 30;
+      E = E.howManyMinute(timeE);
+    }
     
+    return E;
   }
   
   
